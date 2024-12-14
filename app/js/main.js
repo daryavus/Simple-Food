c2873fb24f2fde90e1883a5a6c8ba7be7064d98a
daryavus <vus.dasha@gmail.com>
2024-12-14 21:53:57 +0100

    completed the home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 $(function(){
+  $('.feedback__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="feedback__arrows feedback__arrows--left"><use xlink:href="images/sprite.svg#icon-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="feedback__arrows feedback__arrows--right"><use xlink:href="images/sprite.svg#icon-left"></use></svg></button>'
+  });
 
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,33 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+    .pipe(cheerio({
+      run: ($) => {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke"); 
+          $("[style]").removeAttr("style"); 
+      },
+      parserOptions: { xmlMode: true },
+    })
+    )
+    .pipe(replace('&gt;','>'))
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+		.pipe(dest('app/images'));
+}
 
 function browsersync() {
   browserSync.init({
@@ -33,6 +60,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
@@ -74,6 +102,7 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 
@@ -84,5 +113,6 @@ exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
+exports.svgSprites = svgSprites;
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
